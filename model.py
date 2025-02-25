6d8dddfa4c6ae61fad740d537d47a0593adba299
GitTemp123 <onlyforsearching123@gmail.com>
2025-02-25 09:47:16 +0530

    Update model.py

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ for _ in range(10):
     rng = np.random.RandomState(_)
     x = 10 * rng.rand(1000).reshape(-1,1)
     y = 2 * x - 5 + rng.randn(1000).reshape(-1,1)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=70)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=60)
     ########################################################################
     ######
     # Fitting the model
